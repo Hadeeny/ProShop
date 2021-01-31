bf8ecd73d4a1283dd3fbd0b8a9faeff04f187684
hadeeny <oluwasegunadeniyi064@gmail.com>
2021-01-31 12:02:16 -0800

    backend authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
+router.route("/").post(registerUser);
 
 export default router;
