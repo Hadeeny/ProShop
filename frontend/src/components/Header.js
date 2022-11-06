68458da43b38375dd6ef1a8a17626b44a7c7f8f3
Oluwasegun Michael Adeniyi <63579745+Hadeeny@users.noreply.github.com>
2022-11-06 09:26:21 +0100

    Navbar update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
   };
   return (
     <header>
-      <Navbar bg="warning" variant="white" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>ProShop</Navbar.Brand>
